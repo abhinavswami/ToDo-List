e8fab71d08a2200d7d61bb7a071759248ddb6054
Abhinav Swami <abhinav.swami21@gmail.com>
2017-08-18 19:10:36 -0700

    Move Todo to a separate file

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -3,36 +3,19 @@
   <!-- Javascript expressions in Vue are enclosed in double curly brackets.-->
   <p> Completed Tasks: {{todos.filter(todo => {return todo.done === true}).length}}</p>
   <p> Pending: {{todos.filter(todo => {return todo.done === false}).length}}</p>
-  <div class='ui centered card' v-for="todo in todos">
-    <div class='content'>
-      <div class='header'>
-        {{todo.title}}
-      </div>
-
-      <div class='meta'>
-        {{todo.project}}
-      </div>
-
-      <div class='extra content'>
-        <span class='right floated edit icon'>
-          <i class='edit icon'></i>
-        </span>
-      </div>
-    </div>
-    <div class='ui bottom attached green basic button' v-show="todo.done">
-      Completed
-    </div>
-    <div class='ui bottom attached red basic button' v-show="!todo.done">
-      Complete
-    </div>
-    <br/>
-  </div>
+  <!--we are passing the data to the todo component to render the todo list -->
+  <todo v-for="todo in todos" v-bind:todo="todo"></todo>
 </div>
 </template>
 
 <script type="text/javascript">
+import Todo from './Todo'
+
 export default {
-  props: ['todos']
+  props: ['todos'],
+  components: {
+    Todo
+  }
 }
 </script>
 
