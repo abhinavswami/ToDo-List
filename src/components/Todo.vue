c286c6126c833c5699ea8bb62afdc2367498a9da
Abhinav Swami <abhinav.swami21@gmail.com>
2017-08-19 12:57:00 -0700

    Add edit form to todo item

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,6 +1,7 @@
 <template>
 <div class='ui centered card'>
-  <div class='content'>
+  <!-- Todo shown when we are not in editing mode -->
+  <div class="content" v-show="!isEditing">
     <div class='header'>
       {{todo.title}}
     </div>
@@ -10,11 +11,31 @@
     </div>
 
     <div class='extra content'>
-      <span class='right floated edit icon'>
+      <span class='right floated edit icon' v-on:click="showForm">
         <i class='edit icon'></i>
       </span>
     </div>
   </div>
+
+  <!-- Form is visible when we are in editing mode -->
+  <div class='content' v-show="isEditing">
+    <div class='ui form'>
+      <div class='field'>
+        <label>Title</label>
+        <input type='text' v-model="todo.title">
+      </div>
+      <div class='field'>
+        <label>Project</label>
+        <input type='text' v-model="todo.project">
+      </div>
+      <div class='ui two buttons attached buttons'>
+        <button class='ui basic blue button' v-on:click="hideForm">
+        Close X
+        </button>
+      </div>
+    </div>
+  </div>
+
   <div class='ui bottom attached green basic button' v-show="todo.done">
     Completed
   </div>
@@ -27,6 +48,20 @@
 
 <script type="text/javascript">
 export default {
-  props: ['todo']
+  props: ['todo'],
+  data() {
+    return {
+      isEditing: false
+    }
+  },
+  methods: {
+    showForm() {
+      this.isEditing = true
+    },
+    hideForm() {
+      this.isEditing = false
+    }
+
+  }
 }
 </script>
