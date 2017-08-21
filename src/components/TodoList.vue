7652bc246012591a2c8739b1ca794e30a35bb568
Abhinav Swami <abhinav.swami21@gmail.com>
2017-08-20 22:44:51 -0700

    Add event handler to complete todo

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -5,7 +5,7 @@
   <p> Pending: {{todos.filter(todo => {return todo.done === false}).length}}</p>
   <!--we are passing the data to the todo component to render the todo list -->
 
-  <todo v-on:delete-todo="deleteTodo" v-for="todo in todos" v-bind:todo="todo"></todo>
+  <todo v-on:complete-todo="completeTodo" v-on:delete-todo="deleteTodo" v-for="todo in todos" v-bind:todo="todo"></todo>
 </div>
 </template>
 
@@ -21,6 +21,10 @@ export default {
     deleteTodo(todo) {
       const todoIndex = this.todos.indexOf(todo)
       this.todos.splice(todoIndex, 1)
+    },
+    completeTodo(todo) {
+      const todoIndex = this.todos.indexOf(todo)
+      this.todos[todoIndex].done = true
     }
   }
 }
